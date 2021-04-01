7d1d28396dddc79d64940bd2183e81ed92c3cde7
Nikolov2003 <80042817+Nikolov2003@users.noreply.github.com>
2021-04-01 14:36:27 +0300

    Create GraphPy.py

---
+++ GraphPy.py
@@ -0,0 +1,10 @@
+import math
+xa = float(input('Enter xa: '))
+ya = float(input('Enter ya: '))
+xb = float(input('Enter xb: '))
+yb = float(input('Enter yb: '))
+
+    difx = xa - xb
+    dify = ya - yb
+    print('Dif x on A,B = ' + str(difx))
+    print('Dif y on A,B = ' + str(dify))
